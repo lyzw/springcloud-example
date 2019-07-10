093e6545eaff9e00a412f417be8d6612b89226b9
zhouwei <zhouwei@shanlinjinrong.com>
2019-07-10 18:40:53 +0800

    Initial commit

---
+++ springcloud-config-server/src/main/java/com/sapling/example/springcloud/config/server/ConfigServer.java
@@ -0,0 +1,14 @@
+package com.sapling.example.springcloud.config.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wei.zhou
+ * @date 2019/7/9
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+}
